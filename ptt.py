e59d34c14c3ec0a52e7e5b6e5ed767bf6eda18c7
Connor Kooistra <connor@movingmedia.nl>
2021-10-08 16:32:34 +0200

    For now this is done, the controller returns 't' as intended for the PTT. Can be made more efficient/easy to read

--- ptt.py
+++ ptt.py
@@ -1,28 +1,41 @@
 import time
 import board
 import neopixel
+import usb_hid
 from digitalio import DigitalInOut, Pull
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
 
-led = neopixel.NeoPixel(board.NEOPIXEL, 1) # For working with the LED
+# Create led object and give program rest time
+led = neopixel.NeoPixel(board.NEOPIXEL, 1)
+time.sleep(0.5)
 
+# Create keyboard object
+kbd = Keyboard(usb_hid.devices)
+
+# Button logistics
 button = DigitalInOut(board.SWITCH)
 button.switch_to_input(pull=Pull.DOWN)
 button_state = False
 
-time.sleep(1)
+# Fill the led with blue color
 led.fill((0, 0, 255))
 
+# Methods for pressing/releasing the button
+def pressed():
+    led.fill((255, 0, 0))
+    kbd.press(Keycode.T)
+    
+def released():
+    led.fill((0, 0, 255))
+    kbd.release(Keycode.T)
+
+# Main loop for the controller
 while True:
     if button.value and not button_state:
-        led.fill((255, 0, 0))
-        print("Pressing button")
+        pressed()
         button_state = True
 
     if not button.value and button_state:
-        led.fill((0, 0, 255))
-        print("Released button")
+        released()
         button_state = False
-
-
-
-
